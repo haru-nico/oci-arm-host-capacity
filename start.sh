1e14eb39dd4f4ce46d4ad27751afe4d7df9be6cd
haru-nico <106537636+haru-nico@users.noreply.github.com>
2024-02-06 11:40:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-timeout=$((SECONDS + 240))  # 300 giây = 5 phút
+# Thời gian chạy script (tính bằng giây)
+total_runtime=240
 
-while [ $SECONDS -lt $timeout ]; do
+# Thời gian chờ giữa các lần thực thi (tính bằng giây)
+sleep_time=30
+
+# Thời điểm bắt đầu
+start_time=$(date +%s)
+
+while true; do
+    # Thực thi lệnh PHP
     php index.php
-    sleep 15
+
+    # Kiểm tra thời gian chạy
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+
+    # Kiểm tra xem đã đạt được thời gian chạy mong muốn chưa
+    if [ $elapsed_time -ge $total_runtime ]; then
+        break
+    fi
+
+    # Chờ một khoảng thời gian trước khi thực thi lại
+    sleep $sleep_time
 done
